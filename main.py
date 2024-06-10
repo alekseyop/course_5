918f3e0697cf3b3aeff0c4af538c4f9112b6f890
Aleksey <oszn2011@yandex.ru>
2024-06-10 14:58:35 +0300

    create develop

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
+
+from config import ROOT_DIR
+from src.utils import create_or_update_config
+
 
 def main():
-    pass
+    file_path = os.path.join(ROOT_DIR, 'config_db.ini')
+    create_or_update_config(file_path)
+
+
 
 if __name__ == '__main__':
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,43 @@
 import psycopg2
+import configparser
+import os
+
+def create_or_update_config(file_path):
+    config = configparser.ConfigParser()
+
+    if os.path.exists(file_path):
+        user_choice = input(f"Файл конфигурации {file_path} уже существует. Хотите пересоздать его? (y/n): ")
+        if user_choice.lower() != 'y':
+            print("Используется текущий файл конфигурации.")
+            return
+        else:
+            os.remove(file_path)
+
+    dbname = input("Введите имя базы данных(по умолчанию, hh_db_007): ")
+    dbname = dbname if dbname else 'hh_db_007'
+
+    user = input("Введите имя пользователя(по умолчанию, postgres): ")
+    user = user if user else 'postgres'
+
+    password = input("Введите пароль(по умолчанию, 362362): ")
+    password = password if password else '362362'
+
+    host = input("Введите хост(по умолчанию, localhost): ")
+    host = host if host else 'localhost'
+
+    port = input("Введите порт(по умолчанию, 5432): ")
+    port = port if port else '5432'
+
+    config['postgresql'] = {
+        'dbname': dbname,
+        'user': user,
+        'password': password,
+        'host': host,
+        'port': port
+    }
+
+    with open(file_path, 'w') as configfile:
+        config.write(configfile)
+        print(f"Файл конфигурации {file_path} успешно создан.")
+
 
